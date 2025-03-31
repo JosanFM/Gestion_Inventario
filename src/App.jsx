19acc94a87cbf155cb17d139936b486c1338c897
JoseAnFM <jfm0023@alu.medac.es>
2025-03-31 18:09:35 +0200

    Desarrollo listado a falta de peticion a BD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import './App.css'
+import { Listado } from './components/Listado'
 import { Login } from './components/Login'
 
 
 function App() {
 
+  const [listadoState, setListadoState] = useState([]);
+
   return (
     <>
       <Login/>
+      <Listado listadoState = {listadoState} setListadoState = {setListadoState}/>
     </>
   )
 }
