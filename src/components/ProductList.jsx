ba1a8547db22f93da05131265a98c7137c546172
JoseAnFM <jfm0023@alu.medac.es>
2025-04-09 11:09:52 +0200

    Termino funcionalidad de aumentar y disminuir cantidad de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabase/cliente';
 import { DeleteProduct } from './DeleteProduct';
+import { ControlarCantidad } from './ControlarCantidad';
 
 
 export const ProductList = () => {
@@ -30,7 +31,8 @@ export const ProductList = () => {
             }
         };
         fetchProducts();
-    },[]);
+    },[]); // Entre los corchetes pongo cantidad y eliminar para que cuando se modifique eso
+    // se recargue la pagina y no se recargue la pagina solo al inicio!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     if (loading) return <div>Cargando productos...</div>;
 
@@ -41,7 +43,15 @@ export const ProductList = () => {
 
     const handleProductDeleted = (deletedId) => {
         setProducts(products.filter(product => product.id !== deletedId))
-    }
+    };
+
+    const handleCantidadActualizada = (productId, nuevaCantidad) => {
+        setProducts((prevProducts) =>
+            prevProducts.map((product) =>
+                product.id === productId ? { ...product, Cantidad: nuevaCantidad } : product
+            )
+        );
+    };
 
 
 
@@ -80,9 +90,12 @@ export const ProductList = () => {
                         <article key={product.id} className='product-item'>
                             <h3>{product.Nombre}</h3>
                             <p>Precio por unidad: {product.Precio} euros</p>
-                            <p>Cantidad disponible: {product.Cantidad}</p>
 
-                            <button>Editar</button>
+                            <ControlarCantidad 
+                            productId={product.id} 
+                            cantidadInicial={product.Cantidad}
+                            onCantidadActualizada ={handleCantidadActualizada}
+                            />
                             <DeleteProduct productId={product.id} onDelete={handleProductDeleted}/>
                         </article>
                     )
